ec173d556ef615ab51ec534ff5591e77b1369139
Aboubacar Doucoure <yelounak@gmail.com>
2022-01-02 10:50:19 +0100

    Added formik

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
-import styles from './styles.module.css'
+import { Formik } from 'formik'
+import { generateFormContent } from './utils'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default (props) => {
+  const {
+    onSubmit,
+    error,
+  } = props
+
+  const initialValues = (typeof props.initialValues !== 'function') ? props.initialValues : (props.initialValues && props.initialValues())
+  const validationSchema = (typeof props.validationSchema !== 'function') ? props.validationSchema : (props.validationSchema && props.validationSchema())
+
+  return (
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}>
+        {formProps => generateFormContent({ ...formProps, ...props, initialValues, validationSchema })}
+      </Formik>
+      {error ?
+        <div className="alert alert-error mb-4">
+          <div className="flex-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6 mx-2 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>
+            </svg>
+            <label>{error.message}</label>
+          </div>
+        </div>
+        : null}
+    </>
+  )
 }
